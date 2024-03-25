d6f771d67caef971466992a44e8ad18dc8fab6b9
Kahsolt <kahsolt@qq.com>
2024-03-25 17:20:23 +0800

    add env setup for CUDA12

---
+++ environment.cmd
@@ -0,0 +1,19 @@
+REM follow this script to build a toolchain of
+:: python 3.9 + CUDA 12 + torch 2.x
+
+REM install CUDA 12
+:: https://developer.nvidia.com/cuda-toolkit
+REM install VS 20xx components: C++ Program Tools & Universal CRT SDK
+:: https://visualstudio.microsoft.com/zh-hans/downloads/?q=build+tools
+
+conda create -y -n gs python==3.9
+conda activate gs
+python -m pip install -U pip
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install tqdm pyreadline
+pip install plyfile
+
+REM open VS 20xx x64 Native Command Prompt
+SET DISTUTILS_USE_SDK=1
+pip install submodules/diff-gaussian-rasterization
+pip install submodules/simple-knn
